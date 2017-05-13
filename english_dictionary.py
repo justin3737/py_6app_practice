11224544de03cc3d5b2e19b0e503fe6f6efd69a4
Justin Wu <juns3700@gmail.com>
2017-05-13 23:44:37 +0800

    add more function for english directionary app

--- english_dictionary.py
+++ english_dictionary.py
@@ -9,7 +9,24 @@ def list_all_words():
     for key, value in myDict.items():
         print('{} ({})\n{}\n'.format(key,value[1],value[0]))
 
+def new_words():
+    try:
+        word, definition, chinese = input('Enter a new word (word, definition, chinese):').split(',')
+        myDict[word] = [definition, chinese]
+        print('New word "{}" has been added!'.format(word))
+    except ValueError:
+        print('Please make sure your format is correct!')
+    except:
+        print('Something happend unexcepted!')
 
+def remove_words():
+    word = input('Enter a word you want to removed:')
+    if word in myDict:
+        del(myDict[word])
+        print('The keyword "{}" has been removed!'.format(word))
+    else:
+        print('There is no "{}" in dictionary!'.format(word))
+    
 def test_yourself():
     quit_the_test = False
     points = 0
@@ -48,13 +65,19 @@ def run():
         cmd = input("""\nWelcome to your dictionary!
 1) Test your self!
 2) List all words
-3) Exit\n""")
+3) Add new word
+4) Remove word
+5) Exit\n""")
         
         if cmd == '1':
             test_yourself()
         elif cmd == '2':
             list_all_words()
         elif cmd == '3':
+            new_words()
+        elif cmd == '4':
+            remove_words()
+        elif cmd == '5':
             break
 
 
